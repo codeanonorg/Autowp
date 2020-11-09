bd66292600d80d439e0a5674a0e332bbd5225057
Arthur Correnson <arthur.correnson@gmail.com>
2020-11-09 16:46:14 +0100

    Add example programs and notations

--- src/imp.ml
+++ src/imp.ml
@@ -2,24 +2,73 @@ open Logic
 
 type expr = term
 
-type cond =
-  | Eq of expr * expr
-  | Gt of expr * expr
-  | Lt of expr * expr
-  | Ge of expr * expr
-  | GT of expr * expr
-  | Ne of expr * expr
+type cond = formula
 
 type stmt =
   | Aff of string * expr
   | If of cond * stmt
   | IfElse of cond * stmt * stmt
-  | Seq of stmt * stmt
+  | Seqc of stmt * stmt
+  (* TODO : WHILE loop *)
 
 let rec wp (s : stmt) (q : formula) =
   match s with
   | Aff (v, e) ->
     alpha v e q
-  | Seq (s1, s2) ->
+  | Seqc (s1, s2) ->
     wp s1 (wp s2 q)
-  | _ -> assert false
+  | If (c, s) ->
+    Impl (c, wp s q)
+  | IfElse (c, s1, s2) ->
+    let case1 = Impl (c, wp s1 q) in
+    let case2 = Impl (Not c, wp s2 q) in
+    And (case1, case2)
+
+let (===) a b = eq a b
+let (&&&) a b = And (a, b)
+let (|||) a b = Or (a, b)
+let (-->) a b = Or (a, b)
+let (<--) a b = Aff (a, b)
+let seq a b = Seqc (a, b)
+
+let rec mk_seq l = 
+  match l with
+  | [] -> failwith "bad arg mk_seq"
+  | [x] -> x
+  | x::xs -> seq x (mk_seq xs)
+
+let ifThen a b = If (a, b)
+
+let ifElse a b c = IfElse (a, b, c)
+
+
+let ex_prog_swap () =
+  let va = Var "A" in
+  let vb = Var "B" in
+  let vx = Var "x" in
+  let vy = Var "y" in
+  let tmp = Var "tmp" in
+  let pre = And (eq vx va , eq vy vb) in
+  let post = And (eq vx vb , eq vy va) in
+  let prog = mk_seq [
+      "tmp" <-- vx;
+      "x"   <-- vy;
+      "y"   <-- tmp;
+    ] in
+  Impl (pre, wp prog post) |> str_of_form |> print_endline
+
+let ex_prog_max () =
+  let va = Var "a" in
+  let vb = Var "b" in
+  let vr = Var "r" in
+  let pre = top in
+  let post = And (ge vr va, ge vr vb) in
+  let prog =
+    ifElse (ge va vb)
+      ("r" <-- vb)
+      ("r" <-- va)
+  in
+  Impl (pre, wp prog post) |> str_of_form |> print_endline
+
+
+
--- src/logic.ml
+++ src/logic.ml
@@ -3,7 +3,12 @@ type term =
   | Var of string
   | Fun of string * term list
 
-type formula = Pred of string * term list
+type formula =
+  | Pred of string * term list
+  | And of formula * formula
+  | Or of formula * formula
+  | Impl of formula * formula
+  | Not of formula
 
 let eq a b = Pred ("=", [a; b])
 
@@ -17,11 +22,68 @@ let lt a b = Pred ("<", [a; b])
 
 let sum i j t = Fun ("Sum", [i; j; t])
 
-let plus a b = Fun ("+", [a; b])
+let add a b = Fun ("+", [a; b])
+
+let sub a b = Fun ("-", [a; b])
+
+let mul a b = Fun ("*", [a; b])
+
+let div a b = Fun ("/", [a; b])
+
+let top = Pred ("True", [])
+
+let bot = Pred ("False", [])
 
 let rec alpha_term v e = function
   | Var v' -> if v = v' then e else Var v'
   | Fun (s, ts) -> Fun (s, List.map (alpha_term v e) ts)
   | Int i -> Int i
 
-let alpha v e (Pred (p, ts)) = Pred (p, List.map (alpha_term v e) ts)
+let rec alpha v e f =
+  match f with
+  | Pred (p, ts)  -> Pred (p, List.map (alpha_term v e) ts)
+  | Or (t1, t2)   -> Or (alpha v e t1, alpha v e t2)
+  | And (t1, t2)  -> And (alpha v e t1, alpha v e t2)
+  | Impl (t1, t2) -> Impl (alpha v e t1, alpha v e t2)
+  | Not t -> Not (alpha v e t)
+
+let rec str_of_term =
+  function
+  | Var v -> v
+  | Fun (s, ts) -> begin
+      match s, ts with
+      | "+", [a; b] -> Printf.sprintf "(%s + %s)" (str_of_term a) (str_of_term b)
+      | "-", [a; b] -> Printf.sprintf "(%s - %s)" (str_of_term a) (str_of_term b)
+      | "*", [a; b] -> Printf.sprintf "(%s * %s)" (str_of_term a) (str_of_term b)
+      | "/", [a; b] -> Printf.sprintf "(%s / %s)" (str_of_term a) (str_of_term b)
+      | _, _ ->
+        let args = List.fold_left (fun acc t -> acc ^ ", " ^ (str_of_term t)) "" ts
+        in
+        Printf.sprintf "%s(%s)" s args
+    end
+  | Int i -> string_of_int i
+
+let rec str_of_form f =
+  match f with
+  | Pred (p, ts) -> begin
+      match p, ts with
+      | "=", [a; b] -> Printf.sprintf "%s = %s" (str_of_term a) (str_of_term b)
+      | "<=", [a; b] -> Printf.sprintf "%s <= %s" (str_of_term a) (str_of_term b)
+      | "<", [a; b] -> Printf.sprintf "%s < %s" (str_of_term a) (str_of_term b)
+      | ">=", [a; b] -> Printf.sprintf "%s >= %s" (str_of_term a) (str_of_term b)
+      | ">", [a; b] -> Printf.sprintf "%s > %s" (str_of_term a) (str_of_term b)
+      | "True", [] -> Printf.sprintf "True";
+      | "False", [] -> Printf.sprintf "False";
+      | _, _ ->
+        let args = List.fold_left (fun acc t -> acc ^ (str_of_term t) ^ ", ") "" ts
+        in
+        Printf.sprintf "%s(%s)" p args
+    end
+  | Or (f1, f2) ->
+    Printf.sprintf "(%s \\/ %s)" (str_of_form f1) (str_of_form f2)
+  | And (f1, f2) ->
+    Printf.sprintf "(%s /\\ %s)" (str_of_form f1) (str_of_form f2)
+  | Impl (f1, f2) ->
+    Printf.sprintf "(%s -> %s)" (str_of_form f1) (str_of_form f2)
+  | Not f ->
+    Printf.sprintf "!(%s)" (str_of_form f)
